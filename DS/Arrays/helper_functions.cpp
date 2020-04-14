af2e4dfea23e22b3fda867c024dc615fdda59214
shivam06 <shivammahajan6596@gmail.com>
2020-04-14 08:01:15 +0530

    Coded a common file to import helper functions from. Better practice was to create a header file as well.

---
+++ DS/Arrays/helper_functions.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
+#define all(c) c.begin(), c.end()
+#define present(c, e) (c.find(e) != c.end())
+#define tr(c, itr) for (auto itr = c.begin(); itr != c.end(); itr++)
+#define loop(i, n) for (int i = 0; i < n; i++)
+#define pb(c, e) c.push_back(e)
+
+template<typename T> void print_container(T c) {
+	
+	tr (c, itr) {
+		cout << *itr << " ";
+	}
+	cout << endl;
+} 
+
+// int main() {
+// 	vi v{{1, 5, 3, 2}};
+// 	printC(v);
+// }
