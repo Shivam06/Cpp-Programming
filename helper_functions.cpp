0eb815a5c79a672665c2514e4aef0712d8116df6
shivam06 <shivammahajan6596@gmail.com>
2020-04-24 09:06:37 +0530

    Created a common helper functions file

---
+++ helper_functions.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+typedef pair<int, int> ii;
+typedef vector<pair<int,int> > vii;
+
+#define all(c) c.begin(), c.end()
+#define present(c, e) (c.find(e) != c.end())
+#define tr(c, itr) for (auto itr = c.begin(); itr != c.end(); itr++)
+#define loop(i, n) for (int i = 0; i < n; i++)
+#define rloop(i, n) for (int i = n; i>=0; i--)
+#define pb(c, e) c.push_back(e)
+
+template<typename T> void print_container(T c) {
+	
+	tr (c, itr) {
+		cout << *itr << " ";
+	}
+	cout << endl;
+} 
+
+// int main() {
+// 	vi v{{1, 5, 3, 2}};
+// 	printC(v);
+// }
