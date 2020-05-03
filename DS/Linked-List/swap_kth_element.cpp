511cd6637b8c4dd2846f454338be9f6ddde3895d
shivam06 <shivammahajan6596@gmail.com>
2020-05-03 16:14:59 +0530

    (Important) Coded problem to swap kth NODE from beginning and end

--- DS/Linked-List/swap_kth_element.cpp
+++ DS/Linked-List/swap_kth_element.cpp
@@ -1,37 +1,63 @@
 #include "basics.cpp"
 
-void swap_kth_node(Node* head, int k) {
+Node* swap_kth_node(Node* head, int k, int num) {
 	
 	Node* fk = head, *rk = head;
-
+	Node* pre_fk = NULL, *pre_rk = NULL;
 	Node* slow = head, *fast = head;
 	int i = 1;
 	while (i < k) {
+		pre_rk = fast;
 		fast = fast->next;
 		if (fast == NULL)
-			return;
+			return NULL;
 		i++;
 	}
 
-	fk = fast;
+	rk = fast;
 
 	while (fast->next != NULL) {
 		fast = fast->next;
+		pre_fk = slow;
 		slow = slow->next;
 	}
 
-	rk = slow;
-	//cout << rk->data << endl;
-	int temp = fk->data;
-	fk->data = rk->data;
-	rk->data = temp;
+	fk = slow;
+
+	if (k > num/2) {
+		Node* temp;
+		Node* pre_temp = pre_rk;
+		temp = rk;
+
+		pre_rk = pre_fk;
+		rk = fk;
+		pre_fk = pre_temp;
+		fk = temp;
+	}
+
+	if (pre_rk == NULL) {
+		head = fk;
+	} else {
+		pre_rk->next = fk;
+	}
+
+	Node* fk_next = fk->next;
+	if (rk->next != fk) {
+		fk->next = rk->next;
+		pre_fk->next = rk;
+	}
+	else 
+		fk->next = rk;
+
+	rk->next = fk_next;
+	return head;
 }
 
 int main() {
 
-	vi v{{715, 601, 895, 385, 506}};
+	vi v{{1,2,3,4}};
 	Node* n1 = inputLL(v);
-	swap_kth_node(n1, 2);
+	n1 = swap_kth_node(n1, 4, 4);
 	printLL(n1);
 	return 0;
 }
