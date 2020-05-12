10a2dacdbbeab725b1105f2ecc2a8f7e370158a9
shivam06 <shivammahajan6596@gmail.com>
2020-05-12 08:45:35 +0530

    Also coded function to find count of digits

--- Algo/Recursion/sum_of_digits.cpp
+++ Algo/Recursion/sum_of_digits.cpp
@@ -9,6 +9,21 @@ int sumOfDigits(int n)
     return n%10 + sumOfDigits(n/10);
 }
 
+int count_digits(int n) {
+    return n==0? n: count_digits(n/10) + 1;
+}
+
+//Complete this function
+int countDigits(int n)
+{
+   if (n == 0)
+       return 1;
+       
+    return count_digits(n);
+}
+
 int main() {
 	cout << sumOfDigits(49) << endl;
+	cout << countDigits(49) << endl;
+	cout << countDigits(0) << endl;
 }
