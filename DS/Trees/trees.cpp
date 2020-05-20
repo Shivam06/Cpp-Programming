6c51b769a06551cd749c47ef5d5d03bd2c231fd4
shivam06 <shivammahajan6596@gmail.com>
2020-05-20 11:14:14 +0530

    Coded all traversals

---
+++ DS/Trees/trees.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+struct Node {
+	int data;
+	Node* left;
+	Node* right;
+
+	Node(int data) {
+		this->data = data; 
+		this->left = NULL;
+		this->right = NULL;
+	}
+};
+
+
+Node* default_input() {
+	Node* head = new Node(10);
+	head->left = new Node(20);
+	head->right = new Node(30);
+	head->left->right = new Node(40);
+	head->right->left = new Node(50);
+	head->right->right = new Node(60);
+	return head;
+}
